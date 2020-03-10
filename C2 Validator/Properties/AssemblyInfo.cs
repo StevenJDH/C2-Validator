aa9d03942db50f3fd9695d9b93dcc116a9547c1b
Steven Jenkins De Haro <stevendeharo@gmail.com>
2020-03-10 13:34:21 +0100

    Updated version for new fix release

--- C2 Validator/Properties/AssemblyInfo.cs
+++ C2 Validator/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.20031")]
-[assembly: AssemblyFileVersion("1.0.1.20031")]
+[assembly: AssemblyVersion("1.0.2.20032")]
+[assembly: AssemblyFileVersion("1.0.2.20032")]
